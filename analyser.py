42648aca57c4e622c9b93ce9e01ddbc2145d8a13
Luiz <Luiz.correia@Escriba.com.br>
2023-08-30 07:18:06 -0300

    Dev-002 - Adicionando mais dados

--- analyser.py
+++ analyser.py
@@ -5,3 +5,15 @@ def get_tempo_medio_operacao():
 
 def get_total_operacoes():
     return database.total_operacoes()
+
+def get_capital_inicial():
+    return database.capital_inicial()
+
+def get_capital_atual():
+    return database.capital_atual()
+
+def get_quantidade_operacoes_vencedoras():
+    return database.quantidade_operacoes_vencedoras()
+
+def get_taxa_de_acerto():
+    return database.taxa_de_acerto()
